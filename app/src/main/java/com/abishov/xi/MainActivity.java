a2f8f913b5095fa3db4a9ab22cda06973616f62f
Araz Abishov <araz@abishov.com>
2019-11-02 17:04:01 +0100

    (wip) view part of the text editor

--- app/src/main/java/com/abishov/xi/MainActivity.java
+++ app/src/main/java/com/abishov/xi/MainActivity.java
@@ -2,34 +2,80 @@ package com.abishov.xi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.TextView;
+import com.abishov.xi.core.XiConnection;
+import com.abishov.xi.core.XiCore;
+import com.abishov.xi.core.rpc.Call;
+import com.abishov.xi.core.rpc.Parameters;
 import com.abishov.xi.editor.view.EditorView;
+import io.reactivex.Single;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-  private TextView editorView;
+  private EditorView editorView;
+  private CompositeDisposable disposable = new CompositeDisposable();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    Single<XiConnection> xiConnection =
+        XiCore.create(this)
+            .connect()
+            .cache();
+
+    disposable.add(
+        xiConnection.toObservable()
+            .subscribeOn(Schedulers.io())
+            .switchMap(XiConnection::calls)
+            .subscribe(event -> System.out.println("XiEvent: " + event), error -> {
+              System.out.println("Ooops");
+              error.printStackTrace();
+            })
+    );
+//
+    openFile(xiConnection, "resume.md");
+
     editorView = findViewById(R.id.editorview);
-    editorView.setHorizontallyScrolling(true);
-    editorView.setMovementMethod(new ScrollingMovementMethod());
-    editorView.setCursorVisible(true);
-    editorView.setText("This is an example of text rendering on android. This is an example of "
-        + "text rendering on android. This is an example of text rendering on android. "
-        + "text rendering on android. This is an example of text rendering on android. "
-        + "text rendering on android. This is an example of text rendering on android. "
-        + "text rendering on android. This is an example of text rendering on android. "
-        + "text rendering on android. This is an example of text rendering on android. "
-        + "text rendering on android. This is an example of text rendering on android. \n"
-        + "text rendering on android. This is an example of text rendering on android. "
-        + "text rendering on android. This is an example of text rendering on android. "
-        + "text rendering on android. This is an example of text rendering on android. "
-        + "text rendering on android. This is an example of text rendering on android. ");
-    // editorView.setText("This is an example of text rendering on android.");
+//    editorView.setHorizontallyScrolling(true);
+//    editorView.setMovementMethod(new ScrollingMovementMethod());
+//    editorView.setCursorVisible(true);
+//    editorView.setText("This is an example of text rendering on android. This is an example of "
+//        + "text rendering on android. This is an example of text rendering on android. "
+//        + "text rendering on android. This is an example of text rendering on android. "
+//        + "text rendering on android. This is an example of text rendering on android. "
+//        + "text rendering on android. This is an example of text rendering on android. "
+//        + "text rendering on android. This is an example of text rendering on android. "
+//        + "text rendering on android. This is an example of text rendering on android. \n"
+//        + "text rendering on android. This is an example of text rendering on android. "
+//        + "text rendering on android. This is an example of text rendering on android. "
+//        + "text rendering on android. This is an example of text rendering on android. "
+//        + "text rendering on android. This is an example of text rendering on android. ");
+  }
+
+  private void openFile(Single<XiConnection> xiConnection, String fileName) {
+    File file = new File(getFilesDir(), fileName);
+    Call call = Call.newView(0, Parameters.create(file.getAbsolutePath()));
+
+    System.out.println("File path: " + file.getAbsolutePath());
+
+    disposable.add(
+        xiConnection.toObservable()
+            .subscribeOn(Schedulers.io())
+            .flatMapCompletable(connection -> connection.send(call))
+            .subscribe(() -> {
+              System.out.println("Send is complete.");
+            }, error -> {
+              System.out.println("Ooops");
+              error.printStackTrace();
+            })
+
+        /*
+
+         */
+    );
   }
 }
--- app/src/main/java/com/abishov/xi/editor/view/EditorView.java
+++ app/src/main/java/com/abishov/xi/editor/view/EditorView.java
@@ -10,23 +10,26 @@ import android.text.Layout.Alignment;
 import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.HorizontalScrollView;
 import com.abishov.xi.R;
 
-public final class EditorView extends View {
+public final class EditorView extends HorizontalScrollView {
 
   private Layout textLayout;
   private TextPaint textPaint;
   private int[] location = new int[2];
 
-  // private Paint highlightPaint;
-
-  // data
   private CharSequence text;
 
   public EditorView(Context context, @Nullable AttributeSet attrs) {
     super(context, attrs);
 
+    /*
+     * I don't understand what exactly fronted should send to the backend, and what to expect from
+     * backend. The simplest way would be to start from opening the file, and checking out what
+     * backend sends you. Then render the text using your custom view (following unidirectional
+     * data flow architecture approach).
+     * */
     text = "This is an example of text rendering on android. This is an example of "
         + "text rendering on android. This is an example of text rendering on android. "
         + "text rendering on android. This is an example of text rendering on android. "
@@ -43,10 +46,8 @@ public final class EditorView extends View {
     textPaint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
     textPaint.setTextSize(64);
 
-    textLayout = new StaticLayout(text, textPaint, 1024, Alignment.ALIGN_CENTER,
+    textLayout = new StaticLayout(text, textPaint, Integer.MAX_VALUE, Alignment.ALIGN_CENTER,
         1, 1, true);
-
-    System.out.println("TextLayout: " + textLayout.getLineCount());
   }
 
   @Override
@@ -55,41 +56,15 @@ public final class EditorView extends View {
 
     canvas.save();
 
-    // getLocationOnScreen();
-
-    // calculate X and Y coordinates - In this case we want to draw the text in the
-    // center of canvas so we calculate
-    // text height and number of lines to move Y coordinate to center.
-    // float textHeight = getTextHeight(text, textPaint);
-
-    // int numberOfTextLines = textLayout.getLineCount();
-
     getLocationInWindow(location);
 
     int x = location[0];
     int y = location[1];
 
-//    float textYCoordinate = exactCenterY() - ((numberOfTextLines * textHeight) / 2);
-//
-//    // text will be drawn from left
-//    float textXCoordinate = bounds.left;
-
     canvas.translate(x, y);
 
-    // draws static layout on canvas
     textLayout.draw(canvas);
 
     canvas.restore();
-
-    // textLayout.draw(canvas, new Path(), highlightPaint, 0);
   }
-
-  /**
-   * @return text height
-   */
-//  private float getTextHeight(CharSequence text, Paint paint) {
-//    Rect rect = new Rect();
-//    paint.getTextBounds(text, 0, text.length(), rect);
-//    return rect.height();
-//  }
 }
